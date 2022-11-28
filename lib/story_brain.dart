c6b6a595c4e856af968032c475d4f264e1950a87
Leonardo Peixoto <ljpeixoto@gmail.com>
2022-11-28 19:45:20 -0300

    Steps 22, 23, 24, 25: fix getChoice1 and getChoice2, and make go back to first story when choice leads to empty story

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -47,16 +47,16 @@ class StoryBrain {
     _currentStory = _story0;
   }
 
-  String? getStory() {
-    return _currentStory?.storyTitle;
+  String getStory() {
+    return _currentStory!.storyTitle;
   }
 
-  String? getChoice1() {
-    return _currentStory?.choice1;
+  String getChoice1() {
+    return _currentStory!.choice1;
   }
 
-  String? getChoice2() {
-    return _currentStory?.choice2;
+  String getChoice2() {
+    return _currentStory!.choice2;
   }
 
   void nextStory(int choiceNumber) {
@@ -65,17 +65,12 @@ class StoryBrain {
     } else {
       _currentStory = _currentStory?.nextStory2;
     }
+    _currentStory ??= _story0;
   }
 }
-
-//TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
 //TODO: Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
 
 //TODO: Step 12 - Create a method called getChoice2() that returns the text for the first choice2 from _storyData.
-
-//TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
-//TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
 
 //TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
