ed0821a04b2326e218ec69fd050300fef9d4d198
Leonardo Peixoto <ljpeixoto@gmail.com>
2022-11-28 19:22:21 -0300

    Steps 20, 21: implement sequence of stories using _currentStory and nextStory1 and nextStory2 instead of storyNumber

--- lib/story.dart
+++ lib/story.dart
@@ -2,7 +2,12 @@ class Story {
   String storyTitle;
   String choice1;
   String choice2;
+  Story? nextStory1;
+  Story? nextStory2;
 
-  Story(
-      {required this.storyTitle, required this.choice1, required this.choice2});
+  Story({
+    required this.storyTitle,
+    required this.choice1,
+    required this.choice2,
+  });
 }
